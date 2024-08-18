03e775d1f4cf758a5117effa88fe152987444a65
Arnav Priyadarshi <arnavp103@gmail.com>
2024-08-18 09:08:21 -0400

    a pretty easy medium

---
+++ leetcode/211.py
@@ -0,0 +1,43 @@
+# 211 Design Add and Search Words Data Structure
+
+
+class WordDictionary:
+    def __init__(self):
+        self.root = {}
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for letter in word:
+            if letter not in node:
+                node[letter] = {}
+            node = node[letter]
+        node["sentinel"] = True
+
+    def search(self, word: str) -> bool:
+        return self.search_helper(word, self.root)
+
+    def search_helper(self, word: str, node: dict) -> bool:
+        if len(word) == 0:
+            return "sentinel" in node
+
+        letter = word[0]
+        if letter != ".":
+            if letter not in node:
+                return False
+            return self.search_helper(word[1:], node[letter])
+
+        for possible_node in node.values():
+            # if possible_node is a sentinel, then skip it
+            if possible_node == True:
+                continue
+
+            if self.search_helper(word[1:], possible_node):
+                return True
+
+        return False
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
---
+++ leetcode/264.py
@@ -0,0 +1,37 @@
+# 264 Ugly Number II
+
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        # you start with 0 2s, 0 3s, and 0 5s
+        # the first ugly number is 1
+        # then you have to keep adding the minimum of the previous ugly number
+
+        ugly_numbers = [1]
+
+        # keep an index of the last place
+        # you incremented a 2, 3, or 5
+        twos = 0
+        threes = 0
+        fives = 0
+
+        for i in range(1, n):
+            prev = ugly_numbers[-1]
+
+            if ugly_numbers[twos] * 2 <= prev:
+                twos += 1
+
+            if ugly_numbers[threes] * 3 <= prev:
+                threes += 1
+
+            if ugly_numbers[fives] * 5 <= prev:
+                fives += 1
+
+            mult_two = ugly_numbers[twos] * 2
+            mult_three = ugly_numbers[threes] * 3
+            mult_five = ugly_numbers[fives] * 5
+
+            ugly_numbers.append(min(mult_two, mult_three, mult_five))
+
+        print(ugly_numbers, len(ugly_numbers))
+        return ugly_numbers[-1]
